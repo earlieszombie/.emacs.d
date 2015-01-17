ae529f50726f426f5a9ab2b0400d0e841b119adc
Fred Boldireff <earlieszombie@gmail.com>
2015-01-17 11:18:36 +0100

    revert from sanityinc

--- init.el
+++ init.el
@@ -31,10 +31,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
  '(custom-safe-themes
    (quote
-    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" default)))
+    ("1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" default)))
  '(display-battery-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -43,5 +42,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:height 160 :family "Ubuntu Mono derivative Powerline")))))
- 
+
 (setq smartparens-mode t)
