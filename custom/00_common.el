529d23a20a298f40a258a380f1582b6ad9d11a28
Fred Boldireff <earlieszombie@gmail.com>
2015-01-28 10:58:46 +0100

    common additions

--- custom/00_common.el
+++ custom/00_common.el
@@ -2,3 +2,13 @@
 (setq x-select-enable-clipboard t)
 (global-set-key (kbd "M-(") "{")
 (global-set-key (kbd "M-5") "[")
+
+;; Don't show the startup screen
+(setq inhibit-startup-message t)
+
+;; Display line and column numbers
+(setq line-number-mode t)
+(setq column-number-mode t)
+
+;; Make sure all backup files only live in one place
+(setq backup-directory-alist '(("." . "~/.emacs/backups")))
